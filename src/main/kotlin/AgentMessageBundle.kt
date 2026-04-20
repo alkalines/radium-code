1a212d637db7bd36625fb373be89686b52837a76
Gabriel-Hiss <bielpaes912@gmail.com>
2026-04-19 22:14:45 -0300

    feat(ui): create mock chat It only works the basic feature like typing and selectiong a model

--- src/main/kotlin/AgentMessageBundle.kt
+++ src/main/kotlin/AgentMessageBundle.kt
@@ -5,10 +5,10 @@ import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.PropertyKey
 import java.util.function.Supplier
 
-private const val BUNDLE = "messages.MyMessageBundle"
+private const val BUNDLE = "messages.AgentMessageBundle"
 
-internal object MyMessageBundle {
-    private val instance = DynamicBundle(MyMessageBundle::class.java, BUNDLE)
+internal object AgentMessageBundle {
+    private val instance = DynamicBundle(AgentMessageBundle::class.java, BUNDLE)
 
     @JvmStatic
     fun message(key: @PropertyKey(resourceBundle = BUNDLE) String, vararg params: Any?): @Nls String {
